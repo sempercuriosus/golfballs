40d7536ff05d083e9218e773c25497dd85d49b45
Eric <vespers-garages.0n@icloud.com>
2025-04-03 21:28:20 -0400

    updated connection profile

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,49 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+const DB = mongoose.connection; // elevating this to be accessible to both open and close
+const WriteFile = require('../helpers/docWrite');
 
-const connectionString = process.env.CONNECTION_STRING;
-const dbName = process.env.DB_NAME;
+function CONNECT_DB() {
+  const URI = process.env.CONNECTION_STRING;
+  const DOCUMENT = process.env.DB_NAME;
+  const CONNECTION = URI + DOCUMENT;
 
-console.log(connectionString + dbName);
+  try {
+    mongoose.connect(CONNECTION);
 
-mongoose.connect(connectionString + dbName || '');
+    DB.once('open', () => {
+      console.log('Connected to Database');
+    });
+  } catch (error) {
+    console.error('!!! - Database Error - Connect:', error);
+  }
 
-module.exports = mongoose.connection;
+  try {
+    DB.on('disconnected', () => {
+      console.log('Disconnected from Database');
+    });
+  } catch (error) {
+    console.error('!!! - Database Error - Disconnect:', error);
+  }
+}
+
+async function CLOSE_DB() {
+  try {
+    console.log('Close Connection Received');
+
+    // This should not also GET the data, so where is the best spot for that?
+    // If this did GET the data then there would be no real SOC here...
+
+    WriteFile('THIS IS A TEST FROM THE CONNECTION');
+
+    DB.close(() => {
+      console.log('Closing Database Connection');
+    });
+  } catch (error) {
+    console.error('!!! - Database Error - Close:', error);
+  }
+
+  process.exit(1);
+}
+module.exports = { CONNECT_DB, CLOSE_DB };
 
