b0ed9a1f22adbc57d78f0602811131808117699c
Eric <vespers-garages.0n@icloud.com>
2025-04-07 16:08:19 -0400

    init on the frontend -- new create react app and components I need are included

---
+++ client/golfball-manager/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Dashboard from './components/dashboard';
+import ListItem from './components/list-item';
+
+function App() {
+  return (
+    <>
+      <Dashboard />
+    </>
+  );
+}
+
+export default App;
+
---
+++ client/golfball-manager/src/components/dashboard.jsx
@@ -0,0 +1,10 @@
+function Dashboard() {
+  return (
+    <div>
+      <p> Dashboard Display Test </p>
+    </div>
+  );
+}
+
+export default Dashboard;
+
