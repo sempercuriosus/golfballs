782482e768fb3503a7dd9c438a912438e0bceef3
Eric <vespers-garages.0n@icloud.com>
2025-04-07 17:34:02 -0400

    created links config file

---
+++ client/golfball-manager/src/config/links.js
@@ -0,0 +1,9 @@
+const links = {
+  // Dashboard
+  ballCountApi: '/api/v1/dashboard/countGolfBalls',
+  topBallsApi: '/api/v1/dashboard/topballs',
+  lifetimeAverageApi: '/api/v1/dashboard/lifetimeBallAverage',
+};
+
+export default links;
+
