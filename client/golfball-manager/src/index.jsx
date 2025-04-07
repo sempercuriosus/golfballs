3014be201bf5973458d718e2af4de51310720f0e
Eric <vespers-garages.0n@icloud.com>
2025-04-07 17:41:30 -0400

    jumped the gun on the router. this is an SPA

--- client/golfball-manager/src/index.jsx
+++ client/golfball-manager/src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root'),
@@ -10,9 +9,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    {/* <BrowserRouter> */}
     <App />
-    {/* </BrowserRouter> */}
   </React.StrictMode>,
 );
 
