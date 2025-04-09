ecdc4b3e2e63bfe6c4f4ca6b6367d4fe7bbf5dea
Eric <vespers-garages.0n@icloud.com>
2025-04-09 13:39:01 -0400

    added api link

--- client/golfball-manager/src/config/links.js
+++ client/golfball-manager/src/config/links.js
@@ -3,6 +3,7 @@ const links = {
   ballCountApi: '/api/v1/dashboard/countGolfBalls',
   topBallsApi: '/api/v1/dashboard/topballs',
   lifetimeAverageApi: '/api/v1/dashboard/lifetimeBallAverage',
+  getAllBallsApi: '/api/v1/golfballs/',
 };
 
 export default links;
