d9d8f5e741b4f51c40e3ab45dd36081f5bcce1ae
Eric <vespers-garages.0n@icloud.com>
2025-04-09 13:40:09 -0400

    added headers to sections

--- client/golfball-manager/src/App.jsx
+++ client/golfball-manager/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css';
 import Dashboard from './components/dashboard';
-import ListItem from './components/list-item';
+import BallList from './components/ball-list';
 
 function App() {
   return (
     <>
+      <h4>Dashboard</h4>
+
       <Dashboard />
+
+      <h4>List Of Golf Balls</h4>
+
+      <BallList />
     </>
   );
 }
